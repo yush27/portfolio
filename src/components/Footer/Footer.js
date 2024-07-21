06df91a3ba09830ddc6ca144fb7a0fdeff79fc9c
Pratyush Kumar <pratyushkr.iiit@gmail.com>
2024-07-21 15:54:47 +0530

    uploads

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/yush27/portfolio'
+      className='link footer__link'
+    >
+      By Pratyush Kumar
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,68 @@
+const header = {
+  homepage: '.',
+  title: 'Home',
+}
+
+const about = {
+  name: 'Pratyush Kumar',
+  role: 'Computer Science Student at IIIT Kottayam',
+  description:
+    '',
+  resume: 'https://drive.google.com/file/d/18wjyFmCbuQ7T_GN3LKkeoZT7db46NVnZ/view?usp=sharing',
+  social: {
+    linkedin: 'https://linkedin.com/in/yush27',
+    github: 'https://github.com/yush27',
+  },
+}
+
+const projects = [
+  {
+    name: 'Recipe Search and Management',
+    description:
+      '',
+    stack: ['HTML, CSS, Java Script'],
+    sourceCode: 'https://github.com/yush27/Recipe-Search-and-Management',
+    livePreview: 'https://yush27.github.io/Recipe-Search-and-Management/',
+  },
+  {
+    name: '2048_Game',
+    description:
+      '',
+    stack: ['C++','&', 'Data Structures'],
+    sourceCode: 'https://github.com/yush27/2048_Game',
+    livePreview: 'https://github.com/yush27/2048_Game',
+  },
+  {
+    name: 'portfolio',
+    description:
+      '',
+    stack: ['HTML', 'CSS', 'React JS'],
+    sourceCode: 'https://github.com/yush27/portfolio',
+    livePreview: '.',
+  },
+]
+
+const skills = [
+  'c/c++',
+  'Data Structures',
+  'Algorithms',
+  'Python',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React JS',
+  'Node JS',
+  'Express JS',
+  'Numpy',
+  'Pandas',
+  'Git',
+  'AWS',
+  'MySQL',
+  'MongoDB',
+]
+
+const contact = {
+  email: 'pratyushkr.iiit@mail.com',
+}
+
+export { header, about, projects, skills, contact }
